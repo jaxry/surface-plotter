76e96e3222f9a019a0ae4e2c16855f51865b7a13
jaxry <cabot696@gmail.com>
2017-11-01 15:07:56 -0600

    fix: better EquationInput error checking

--- src/components/EquationInput.js
+++ src/components/EquationInput.js
@@ -1,27 +1,33 @@
 import { createElem, buildDomTree } from '../util';
 
 function functionFromEquation(inputs, equation) {
+  equation = equation.replace(/\^/g, '**'); // replace ^ with the exponentiation operator
+
   const eq = new Function(`
     const ${
       Object.getOwnPropertyNames(Math)
       .map(n => `${n.toLowerCase()}=Math.${n}`)
       .join(',')
     };
+    const ln = Math.log;
 
-    return (${inputs.join(',')}) => ${equation} || 0;
+    return (${inputs.join(',')}) => ${equation};
   `)();
 
-  eq(); // call equation to check for reference errors
+  const type = typeof eq();
+
+  if (type !== 'number' && type !== 'undefined') {
+    throw TypeError;
+  }
 
   return eq;
-
 }
 
 export default class {
   constructor(name, equationInputs, onInput) {
     this.equationInputs = equationInputs;
 
-    this.textarea = createElem('textarea');
+    this.textarea = createElem('textarea', {spellcheck: false});
 
     this.textarea.addEventListener('input', () => {
       this._eval();
@@ -35,7 +41,7 @@ export default class {
       ]
     );
 
-    this.domElement.title = "Constants: e, pi\nFunctions: abs(x), acos(x), acosh(x), asin(x), asinh(x), atan(x), atanh(x), atan2(y, x), ceil(x), cos(x), cosh(x), exp(x), floor(x), log(x), pow(x, y), round(x), sign(x), sin(x), sinh(x), sqrt(x), tan(x), tanh(x)";
+    this.domElement.title = 'Constants: e, pi\nFunctions: abs(x), acos(x), acosh(x), asin(x), asinh(x), atan(x), atanh(x), atan2(y, x), ceil(x), cos(x), cosh(x), exp(x), floor(x), log(x), pow(x, y), round(x), sign(x), sin(x), sinh(x), sqrt(x), tan(x), tanh(x)';
   }
 
   _eval() {
